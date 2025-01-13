3897332702b8cd4504dac1de067b695965d55966
mikhailpopechitelev <popechitelevm@mail.ru>
2025-01-13 15:39:03 +0400

    3 коммит

--- LR1/VRASMV_CONST.py
+++
@@ -1,3 +0,0 @@
-VRASMV_URL = "https://vocal-remover-audio-separator-music-voice.p.rapidapi.com/voice_removal"
-VRASMV_KEY = "0ad7ca6c3cmsh36b5cbd5c91abdcp1a9871jsn485361645cc2"
-VRASMV_HOST = "vocal-remover-audio-separator-music-voice.p.rapidapi.com"
